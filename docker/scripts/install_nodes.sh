d1e45cc1864b4c81aa5605bc2c41e0ef5bcb149b
darlshaw <darlshaw@protonmail.com>
2025-02-21 23:56:38 +0100

    Initial commit with full ComfyUI setup

--- docker/scripts/install_nodes.sh
+++ docker/scripts/install_nodes.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-set -e  # Add error handling
-set -x  # Add debugging
+set -e  # Exit on error
+set -x  # Print commands
+
 # Main nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI-Manager
 git clone https://codeberg.org/Gourieff/comfyui-reactor-node.git custom_nodes/comfyui-reactor-node
 git clone https://github.com/cubiq/ComfyUI_InstantID.git custom_nodes/ComfyUI_InstantID
 git clone https://github.com/Acly/comfyui-inpaint-nodes.git custom_nodes/comfyui-inpaint-nodes
 
-# Workflow-specific nodes from your correction
+# Workflow-specific nodes
 git clone https://github.com/kijai/ComfyUI-KJNodes.git custom_nodes/ComfyUI-KJNodes
 git clone https://github.com/cubiq/ComfyUI_essentials.git custom_nodes/ComfyUI_essentials
 git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack.git custom_nodes/ComfyUI-Impact-Pack
 git clone https://github.com/storyicon/comfyui_segment_anything.git custom_nodes/comfyui_segment_anything
 git clone https://github.com/Fannovel16/comfyui_controlnet_aux.git custom_nodes/comfyui_controlnet_aux
 
-# Install requirements for all nodes
+# Install requirements using pip3
 find custom_nodes/ -name "requirements.txt" -exec pip3 install -r {} \;
